3ce48cb4ad96cd4375130ff0d25c726a692d654b
Brad Caron <bacaron@iu.edu>
2020-08-10 21:30:32 -0400

    Update generateCSV.py

--- generateCSV.py
+++ generateCSV.py
@@ -99,6 +99,10 @@ def main():
 	# depending on what's in the array, rearrange in a specific order I like
 	if all(x in diffusion_measures for x in ['ndi','fa']):
 		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','snr','volume','thickness']
+	elif all(x in diffusion_measures for x in ['ndi','ga']):
+		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','volume','thickness']
+	elif 'ga' in diffusion_measures:
+		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','volume','thickness']
 	elif 'fa' in diffusion_measures:
 		diffusion_measures = ['ad','fa','md','rd','snr','volume','thickness']
 	else:
