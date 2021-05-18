6e574ad7b8bfb960b2bb827262e3ef5ea69b63f5
Brad Caron <bacaron@iu.edu>
2021-05-18 11:18:06 -0400

    Update generateCSV.py

--- generateCSV.py
+++ generateCSV.py
@@ -66,23 +66,48 @@ def main():
 
 	# depending on what's in the array, rearrange in a specific order I like
 	if all(x in diffusion_measures for x in ['noddi_kappa','ga']):
-		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','noddi_kappa','volume','thickness']
 	elif all(x in diffusion_measures for x in ['noddi_kappa','fa']):
-		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','noddi_kappa','volume','thickness']
 	elif all(x in diffusion_measures for x in ['ndi','ga']):
-		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','snr','volume','thickness']
+		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','volume','thickness']
 	elif all(x in diffusion_measures for x in ['ndi','fa']):
-		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','snr','volume','thickness']
+		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','volume','thickness']
 	elif 'ga' in diffusion_measures:
 		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','volume','thickness']
 	elif 'fa' in diffusion_measures:
-		diffusion_measures = ['ad','fa','md','rd','snr','volume','thickness']
+		diffusion_measures = ['ad','fa','md','rd','volume','thickness']
 	elif 'gmd' in diffusion_measures:
-		diffusion_measures = ['gmd','snr']
+		diffusion_measures = ['gmd','volume','thickness']
+	elif 'myelinmap' in diffusion_measures:
+		diffusion_measures = ['myelinmap','volume','thickness']
 	elif 'noddi_kappa' in diffusion_measures:
-		diffusion_measures = ['ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+		diffusion_measures = ['ndi','isovf','odi','noddi_kappa','volume','thickness']
 	else:
-		diffusion_measures = ['ndi','isovf','odi','snr','volume','thickness']
+		diffusion_measures = ['ndi','isovf','odi','volume','thickness']
+		
+	if 'snr' in diffusion_measures:
+		diffusion_measures = diffusion_measures + ['snr']
+	
+# 	# depending on what's in the array, rearrange in a specific order I like
+# 	if all(x in diffusion_measures for x in ['noddi_kappa','ga']):
+# 		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+# 	elif all(x in diffusion_measures for x in ['noddi_kappa','fa']):
+# 		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+# 	elif all(x in diffusion_measures for x in ['ndi','ga']):
+# 		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','ndi','isovf','odi','snr','volume','thickness']
+# 	elif all(x in diffusion_measures for x in ['ndi','fa']):
+# 		diffusion_measures = ['ad','fa','md','rd','ndi','isovf','odi','snr','volume','thickness']
+# 	elif 'ga' in diffusion_measures:
+# 		diffusion_measures = ['ad','fa','md','rd','ga','ak','mk','rk','volume','thickness']
+# 	elif 'fa' in diffusion_measures:
+# 		diffusion_measures = ['ad','fa','md','rd','snr','volume','thickness']
+# 	elif 'gmd' in diffusion_measures:
+# 		diffusion_measures = ['gmd','snr']
+# 	elif 'noddi_kappa' in diffusion_measures:
+# 		diffusion_measures = ['ndi','isovf','odi','noddi_kappa','snr','volume','thickness']
+# 	else:
+# 		diffusion_measures = ['ndi','isovf','odi','snr','volume','thickness']
 
 	# summary statistics measures
 	summary_measures = [ x.split('.')[1].split('tracts_')[1].split('_lh')[0] for x in glob.glob('./tmp/tracts_*_lh.%s.txt' %diffusion_measures[0]) ]
