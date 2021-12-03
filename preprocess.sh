ac5a44174793d6128dadf5eafb98db2465a8c055
Brad Caron <bacaron@iu.edu>
2021-12-03 10:29:10 -0500

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -16,7 +16,7 @@ resliced="./resliced/"
 tmpdir="./tmp/"
 
 # copy freesurfer directory here
-[ ! -d ./output/ ] && cp -R ${freesurfer} ./ && chmod -R +w ./output
+[ ! -d ./output/ ] && cp -R ${freesurfer} ./ && chmod -R +rw ./output
 freesurfer="./output"
 
 # export subjects dir for mri_vol2surf call
