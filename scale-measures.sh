affb36d02d3e4501584f981843f94fd2fd512fa8
Brad Caron <bacaron245@gmail.com>
2022-07-03 19:18:29 -0400

    Reason: small updates

--- scale-measures.sh
+++ scale-measures.sh
@@ -8,16 +8,27 @@
 #### parse inputs ####
 # configs
 cortexmap=`jq -r '.cortexmap' config.json`
+lh_annot=`jq -r '.lh_annot' config.json`
+rh_annot=`jq -r '.rh_annot' config.json`
+lh_white=`jq -r '.left_surf' config.json`
+rh_white=`jq -r '.right_surf' config.json`
+aparc_to_use=`jq -r '.fsaparc' config.json`
+freesurfer=`jq -r '.freesurfer' config.json`
 
 # hemispheres
 hemispheres="lh rh"
 
 # filepaths
-cp -R ${cortexmap} ./cortexmap/ && chmod -R +w ./cortexmap/*
+[ ! -d ./cortexmap ] && cp -R ${cortexmap} ./cortexmap/ && chmod -R +w ./cortexmap/*
+[ ! -d ./output ] && cp -R ${freesurfer} ./output/ && chmod -R +w ./output/*
 cortexmap="./cortexmap/"
 funcdir="${cortexmap}/func"
 surfdir="${cortexmap}/surf"
 labeldir="${cortexmap}/label"
+freesurfer="./output/"
+
+# set subjects dir for freesurfer to pwd
+export SUBJECTS_DIR=./
 
 # metrics
 # identify measures to loop through later
@@ -45,4 +56,34 @@ do
 			fi
 		fi
 	done
+done
+
+# if parcellation exists, generate annot files
+for hemi in $hemispheres
+do
+  echo "converting files for ${hemi}"
+  parc=$(eval "echo \$${hemi}_annot")
+  white=$(eval "echo \$${hemi}_white")"/"${hemi}".white.gii"
+
+  # check if white exists
+  for i in ${white}
+  do
+    if [[ ! "${i}" == *"inflated"* ]]; then
+      white=${i}
+    fi
+  done
+
+  # convert surface parcellations that came from multi atlas transfer tool
+  if [[ ! ${parc} == 'null' ]]; then
+    #### convert annotation files to useable label giftis ####
+    [ ! -f ${hemi}.parc.label.gii ] && mris_convert --annot ${parc} \
+      ${white} \
+      ${hemi}.parc.label.gii
+
+    #### set map names ####
+    wb_command -set-map-names ${hemi}.parc.label.gii -map 1 "${hemi}_parc"
+
+    #### convert to freesurfer .annot file ####
+    [ ! -f ./${hemi}.parc.annot ] && mris_convert --annot ${hemi}.parc.label.gii ${white} ./${hemi}.parc.annot
+  fi
 done
