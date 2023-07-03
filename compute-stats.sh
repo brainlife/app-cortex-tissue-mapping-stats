f151b2ee8f08ca7c207d4b35f4dd4e10c8de0474
Brad Caron <bacaron@utexas.edu>
2023-07-03 15:17:48 -0500

    reason: small fixes

--- compute-stats.sh
+++ compute-stats.sh
@@ -7,7 +7,6 @@ set -e
 
 #### parse inputs ####
 # configs
-# cortexmap=`jq -r '.cortexmap' config.json`
 lh_annot=`jq -r '.lh_annot' config.json`
 rh_annot=`jq -r '.rh_annot' config.json`
 lh_white=`jq -r '.left_surf' config.json`
@@ -19,26 +18,11 @@ freesurfer=`jq -r '.freesurfer' config.json`
 hemispheres="lh rh"
 
 # filepaths
-# [ ! -d ./cortexmap ] && cp -R ${cortexmap} ./cortexmap/ && chmod -R +w ./cortexmap/*
 [ ! -d ./output ] && cp -R ${freesurfer} ./output/ && chmod -R +w ./output/*
-# cortexmap="./cortexmap/"
-# funcdir="${cortexmap}/func"
-# surfdir="${cortexmap}/surf"
-# labeldir="${cortexmap}/label"
 freesurfer="./output/"
 
 # set subjects dir for freesurfer to pwd
 export SUBJECTS_DIR=./
-
-# identify measures to loop through later
-# tmp_measures=(`find cortexmap/func/lh.*`)
-# measures=""
-# for i in ${tmp_measures[*]}
-# do
-#   measures=$measures" "`echo $i | grep -o -P '(?<=cortexmap/func/lh.).*(?=.func.gii)'`
-# done
-# measures=(`echo ${measures}`)
-# echo "measures to loop through:${measures[*]}"
 
 # identify parcellations to use
 parcellations="aparc aparc.a2009s"
@@ -55,28 +39,12 @@ for parcs in ${parcellations[*]}
 do
 	for hemi in ${hemispheres}
 	do
-    # set variable to specific file path if parcs == parc, else just use the name of the freesurfer parcellation
-    if [[ ${parcs} == "parc" ]]; then
-      input=${hemi}.parc.annot
-    else
-      input=${parcs}
-    fi
-
-	# 	# loop through measures
-	# 	for (( j=0; j<${#measures[*]}; j++ ))
-	# 	do
-    #   # set variable outname to make easier to write out
-    #   outname=${parcs}.${hemi}.${measures[$j]}
-
-    #   # this compute the statistics within each parcel of the parcellation for the measure. it will exclude the 0 parcel, which should always be excluded vertices
-	# 	  [ ! -f ./${outname}.txt ] && mri_segstats --annot ./output/ ${hemi} ${input} --i ./cortexmap/func/${hemi}.${measures[$j]}.func.gii --surf white --excludeid 0 --o ./${outname}.txt
-
-	# 	  # convert table to csv
-	# 	  [ ! -f ./${outname}_tailed.txt ] && tail ./${outname}.txt -n +56 > ./${outname}_tailed.txt
-	# 	  # Index SegId NVertices StructName Mean StdDev Min Max Range
-	# 	  [ ! -f ./${outname}_tailed_subselected.txt ] && awk '{print $2,$3,$5,$6,$7,$8,$9,$10,$11,$12,$13}' ./${outname}_tailed.txt > ./${outname}_tailed_subselected.txt
-	# 	  [ ! -f ./${outname}.csv ] && sed 's/ *$//' ./${outname}_tailed_subselected.txt > ./${outname}_tailed_subselected_comma.txt && sed 's/ \+/,/g' ./${outname}_tailed_subselected_comma.txt > ./${outname}.csv
-	# 	done
+	    # set variable to specific file path if parcs == parc, else just use the name of the freesurfer parcellation
+		if [[ ${parcs} == "parc" ]]; then
+			input=${hemi}.parc.annot
+		else
+			input=${parcs}
+		fi
 
 		# compute anatomical stats for parcellation
 		anat_outname=${parcs}.${hemi}".anatomical"
